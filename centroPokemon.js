eaca845df89af78d87298bc1b7e6215495bc95c6
Rica003 <rica.COortega03@gmail.com>
2024-09-02 14:28:35 -0300

    Meu Segundo 2º commit

---
+++ centroPokemon.js
@@ -0,0 +1,34 @@
+/*let nomesPokemon = ["Pikachu" , "Chamander", "Bulbassaur"]
+let timePokemon = [
+  ["pikachu", "M", 1],
+  ["Chamander", "F", 3]
+]
+
+
+console.log(" O pokemon " + timePokemon[1][0] + " é do sexo " +  timePokemon[1][1] + 
+" e está no nível " + timePokemon[1][2] )*/
+
+let pokemonNames = ["Poochvena", "Zigzagoon", "Dragonite", "Wurmple"];
+let pokemonLv = [2, 3, 5];
+let pokemonGender = ["M", "F"];
+let pokemonHp = [7, 13, 15, 24, 25];
+let pokemonCenter = [
+    ["Poochvena", "Zigzagoon", "Dragonite", "Wurmple"],
+    [2, 3, 5],
+    ["M", "F"],
+    [7, 13, 15, 24, 25]
+];
+
+console.log("Pokemon: " + pokemonCenter[0][0] + "\n" + "Lv" + pokemonCenter[1][0] + "\n" + "Gender: " + pokemonCenter[2][0] + "\n" + "HP: " + pokemonCenter[3][1] + "/13" + "\n" + "Cadastrado com Sucesso");
+
+console.log("Pokemon: " + pokemonCenter[0][1] + "\n" + "Lv" + pokemonCenter[1][0] + "\n" + "Gender: " + pokemonCenter[2][1] + "\n" + "HP: " + pokemonCenter[3][1] + "/13" + "\n" + "Cadastrado com Sucesso");
+
+console.log("Pokemon: " + pokemonCenter[0][2] + "\n" + "Lv" + pokemonCenter[1][2] + "\n" + "Gender: " + pokemonCenter[2][0] + "\n" + "HP: " + pokemonCenter[3][4] + "/25" + "\n" + "Cadastrado com Sucesso");
+
+console.log("Pokemon: " + pokemonCenter[0][2] + "\n" + "Lv" + pokemonCenter[1][2] + "\n" + "Gender: " + pokemonCenter[2][1] + "\n" + "HP: " + pokemonCenter[3][3] + "/24" + "\n" + "Cadastrado com Sucesso");
+
+console.log("Pokemon: " + pokemonCenter[0][2] + "\n" + "Lv" + pokemonCenter[1][2] + "\n" + "Gender: " + pokemonCenter[2][1] + "\n" + "HP: " + pokemonCenter[3][3] + "/24" + "\n" + "Cadastrado com Sucesso");
+
+console.log("Pokemon: " + pokemonCenter[0][0] + "\n" + "Lv" + pokemonCenter[1][1] + "\n" + "Gender: " + pokemonCenter[2][1] + "\n" + "HP: " + pokemonCenter[3][2] + "/15" + "\n" + "Cadastrado com Sucesso");
+
+console.log("Pokemon: " + pokemonCenter[0][3] + "\n" + "Lv" + pokemonCenter[1][0] + "\n" + "Gender: " + pokemonCenter[2][0] + "\n" + "HP: " + pokemonCenter[3][0] + "/14" + "\n" + "Cadastrado com Sucesso");
